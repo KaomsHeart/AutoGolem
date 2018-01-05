9aed63c6dfb9734c61804acc79d76b042bebab3b
KaomsHeart <marc.linke@tutanota.com>
2018-01-05 21:35:12 +0100

    first working version

---
+++ src/AutoGolem.cs
@@ -0,0 +1,139 @@
+﻿using PoeHUD.Controllers;
+using PoeHUD.Plugins;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.RemoteMemoryObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AutoGolem
+{
+    internal class AutoGolem : BaseSettingsPlugin<AutoGolemSettings>
+    {
+        private IngameData data;
+        private bool isTown;
+        private KeyboardHelper keyboard;
+
+        public override void Initialise()
+        {
+            PluginName = "Auto Golem";
+
+            OnSettingsToggle();
+            Settings.Enable.OnValueChanged += OnSettingsToggle;
+        }
+
+        private void OnSettingsToggle()
+        {
+            try
+            {
+                if (Settings.Enable.Value)
+                {
+                    GameController.Area.OnAreaChange += OnAreaChange;
+                    GameController.Area.RefreshState();
+
+                    keyboard = new KeyboardHelper(GameController);
+
+                    var checkThread = new Thread(CheckThread) { IsBackground = true };
+                    checkThread.Start();
+                }
+                else
+                {
+                    GameController.Area.OnAreaChange -= OnAreaChange;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void OnAreaChange(AreaController area)
+        {
+            if (Settings.Enable.Value)
+            {
+                data = GameController.Game.IngameState.Data;
+                isTown = area.CurrentArea.IsTown;
+            }
+        }
+
+        private void CheckThread()
+        {
+            while (Settings.Enable.Value)
+            {
+                GolemMain();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GolemMain()
+        {
+            if (GameController == null || GameController.Window == null || GameController.Game.IngameState.Data.LocalPlayer == null || GameController.Game.IngameState.Data.LocalPlayer.Address == 0x00)
+                return;
+
+            if (!GameController.Window.IsForeground())
+                return;
+
+            if (!GameController.Game.IngameState.Data.LocalPlayer.IsValid)
+                return;
+
+            var playerLife = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>();
+            if (playerLife == null || isTown)
+                return;
+
+            List<int> golems = data.LocalPlayer.GetComponent<Actor>().Minions;
+
+            int countChaosGolem = 0;
+            int countFireGolem = 0;
+            int countIceGolem = 0;
+            int countLightningGolem = 0;
+            int countStoneGolem = 0;
+
+            try
+            {
+                foreach (var golemId in golems)
+                {
+                    if (data.EntityList.EntitiesAsDictionary.ContainsKey(golemId))
+                    {
+                        var golemPathString = data.EntityList.EntitiesAsDictionary[golemId].Path;
+
+                        if (golemPathString.Contains("ChaosElemental"))
+                            countChaosGolem++;
+                        if (golemPathString.Contains("FireElemental"))
+                            countFireGolem++;
+                        if (golemPathString.Contains("IceElemental"))
+                            countIceGolem++;
+                        if (golemPathString.Contains("LightningGolem"))
+                            countLightningGolem++;
+                        if (golemPathString.Contains("RockGolem"))
+                            countStoneGolem++;
+                    }
+                }
+
+                if (Settings.ChaosGolem.Value && countChaosGolem < Settings.ChaosGolemMax.Value)
+                {
+                    keyboard.KeyPressRelease(Settings.ChaosGolemKeyPressed.Value);
+                }
+                if (Settings.FireGolem.Value && countFireGolem < Settings.FireGolemMax.Value)
+                {
+                    keyboard.KeyPressRelease(Settings.FireGolemKeyPressed.Value);
+                }
+                if (Settings.IceGolem.Value && countIceGolem < Settings.IceGolemMax.Value)
+                {
+                    keyboard.KeyPressRelease(Settings.IceGolemKeyPressed.Value);
+                }
+                if (Settings.LightningGolem.Value && countLightningGolem < Settings.LightningGolemMax.Value)
+                {
+                    keyboard.KeyPressRelease(Settings.LightningGolemKeyPressed.Value);
+                }
+                if (Settings.StoneGolem.Value && countStoneGolem < Settings.StoneGolemMax.Value)
+                {
+                    keyboard.KeyPressRelease(Settings.StoneGolemKeyPressed.Value);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            
+        }
+    }
+
+}
---
+++ src/AutoGolemSettings.cs
@@ -0,0 +1,72 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System.Windows.Forms;
+
+namespace AutoGolem
+{
+    internal class AutoGolemSettings : SettingsBase
+    {
+        public AutoGolemSettings()
+        {
+            //plugin itself
+            Enable = false;
+
+            //Golems
+            ChaosGolem = false;
+            ChaosGolemKeyPressed = Keys.T;
+            ChaosGolemMax = new RangeNode<int>(1, 1, 10);
+
+            FireGolem = false;
+            FireGolemKeyPressed = Keys.T;
+            FireGolemMax = new RangeNode<int>(1, 1, 10);
+
+            IceGolem = false;
+            IceGolemKeyPressed = Keys.T;
+            IceGolemMax = new RangeNode<int>(1, 1, 10);
+
+            LightningGolem = false;
+            LightningGolemKeyPressed = Keys.T;
+            LightningGolemMax = new RangeNode<int>(1, 1, 10);
+
+            StoneGolem = false;
+            StoneGolemKeyPressed = Keys.T;
+            StoneGolemMax = new RangeNode<int>(1, 1, 10);
+        }
+
+        //Menu
+        [Menu("Chaos Golem", 1)]
+        public ToggleNode ChaosGolem { get; set; }
+        [Menu("Skill Hotkey", 10, 1)]
+        public HotkeyNode ChaosGolemKeyPressed { get; set; }
+        [Menu("Max Golems", 11, 1)]
+        public RangeNode<int> ChaosGolemMax { get; set; }
+
+        [Menu("Fire Golem", 2)]
+        public ToggleNode FireGolem { get; set; }
+        [Menu("Skill Hotkey", 20, 2)]
+        public HotkeyNode FireGolemKeyPressed { get; set; }
+        [Menu("Max Golems", 21, 2)]
+        public RangeNode<int> FireGolemMax { get; set; }
+
+        [Menu("Ice Golem", 3)]
+        public ToggleNode IceGolem { get; set; }
+        [Menu("Skill Hotkey", 30, 3)]
+        public HotkeyNode IceGolemKeyPressed { get; set; }
+        [Menu("Max Golems", 31, 3)]
+        public RangeNode<int> IceGolemMax { get; set; }
+
+        [Menu("Lightning Golem", 4)]
+        public ToggleNode LightningGolem { get; set; }
+        [Menu("Skill Hotkey", 40, 4)]
+        public HotkeyNode LightningGolemKeyPressed { get; set; }
+        [Menu("Max Golems", 41, 4)]
+        public RangeNode<int> LightningGolemMax { get; set; }
+
+        [Menu("Stone Golem", 5)]
+        public ToggleNode StoneGolem { get; set; }
+        [Menu("Skill Hotkey", 50, 5)]
+        public HotkeyNode StoneGolemKeyPressed { get; set; }
+        [Menu("Max Golems", 51, 5)]
+        public RangeNode<int> StoneGolemMax { get; set; }
+    }
+}
