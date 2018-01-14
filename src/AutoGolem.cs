1963e4ff0e2291476dd9818d3e1d2b15209c33ba
Kaoms Heart <marc.linke@tutanota.com>
2018-01-14 23:18:04 +0100

    better handling of weapon swap
    * added setting to choose the skill slot that the golem skill is using
    * now it is checked if the selected skill slot really has the golem skill (avoids casting other skills when using weapon swap)

--- src/AutoGolem.cs
+++ src/AutoGolem.cs
@@ -2,8 +2,11 @@
 using PoeHUD.Plugins;
 using PoeHUD.Poe.Components;
 using PoeHUD.Poe.RemoteMemoryObjects;
+using SharpDX;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace AutoGolem
@@ -13,6 +16,8 @@ namespace AutoGolem
         private IngameData data;
         private bool isTown;
         private KeyboardHelper keyboard;
+        private Stopwatch stopwatch = new Stopwatch();
+        private int highlightSkill = 0;
 
         public override void Initialise()
         {
@@ -20,6 +25,42 @@ namespace AutoGolem
 
             OnSettingsToggle();
             Settings.Enable.OnValueChanged += OnSettingsToggle;
+
+            Settings.ChaosGolemConnectedSkill.OnValueChanged += OnChaosGolemConnectedSkillChanged;
+            Settings.FireGolemConnectedSkill.OnValueChanged += OnFireGolemConnectedSkillChanged;
+            Settings.IceGolemConnectedSkill.OnValueChanged += OnIceGolemConnectedSkillChanged;
+            Settings.LightningGolemConnectedSkill.OnValueChanged += OnLightningGolemConnectedSkillChanged;
+            Settings.StoneGolemConnectedSkill.OnValueChanged += OnStoneGolemConnectedSkillChanged;
+        }
+
+        private void OnChaosGolemConnectedSkillChanged()
+        {
+            highlightSkill = Settings.ChaosGolemConnectedSkill.Value - 1;
+            stopwatch.Restart();
+        }
+
+        private void OnFireGolemConnectedSkillChanged()
+        {
+            highlightSkill = Settings.FireGolemConnectedSkill.Value - 1;
+            stopwatch.Restart();
+        }
+
+        private void OnIceGolemConnectedSkillChanged()
+        {
+            highlightSkill = Settings.IceGolemConnectedSkill.Value - 1;
+            stopwatch.Restart();
+        }
+
+        private void OnLightningGolemConnectedSkillChanged()
+        {
+            highlightSkill = Settings.LightningGolemConnectedSkill.Value - 1;
+            stopwatch.Restart();
+        }
+
+        private void OnStoneGolemConnectedSkillChanged()
+        {
+            highlightSkill = Settings.StoneGolemConnectedSkill.Value - 1;
+            stopwatch.Restart();
         }
 
         private void OnSettingsToggle()
@@ -33,16 +74,36 @@ namespace AutoGolem
 
                     keyboard = new KeyboardHelper(GameController);
 
+                    stopwatch.Reset();
+
                     var checkThread = new Thread(CheckThread) { IsBackground = true };
                     checkThread.Start();
                 }
                 else
                 {
                     GameController.Area.OnAreaChange -= OnAreaChange;
+
+                    stopwatch.Stop();
                 }
             }
             catch (Exception)
             {
+            }
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            if (!Settings.Enable.Value) return;
+
+            if (stopwatch.IsRunning && stopwatch.ElapsedMilliseconds < 5000)
+            {
+                IngameUIElements ingameUiElements = GameController.Game.IngameState.IngameUi;
+                Graphics.DrawFrame(ingameUiElements.SkillBar[highlightSkill].GetClientRect(), 3f, Color.Yellow);
+            }
+            else
+            {
+                stopwatch.Stop();
             }
         }
 
@@ -81,6 +142,7 @@ namespace AutoGolem
 
             try
             {
+                IngameUIElements ingameUiElements = GameController.Game.IngameState.IngameUi;
                 List<int> golems = data.LocalPlayer.GetComponent<Actor>().Minions;
 
                 int countChaosGolem = 0;
@@ -108,23 +170,23 @@ namespace AutoGolem
                     }
                 }
 
-                if (Settings.ChaosGolem.Value && countChaosGolem < Settings.ChaosGolemMax.Value)
+                if (Settings.ChaosGolem.Value && countChaosGolem < Settings.ChaosGolemMax.Value && ingameUiElements.SkillBar[Settings.ChaosGolemConnectedSkill.Value - 1].SkillIconPath.Contains("ChaosElementalSummon"))
                 {
                     keyboard.KeyPressRelease(Settings.ChaosGolemKeyPressed.Value);
                 }
-                if (Settings.FireGolem.Value && countFireGolem < Settings.FireGolemMax.Value)
+                if (Settings.FireGolem.Value && countFireGolem < Settings.FireGolemMax.Value && ingameUiElements.SkillBar[Settings.FireGolemConnectedSkill.Value - 1].SkillIconPath.Contains("FireElementalSummon"))
                 {
                     keyboard.KeyPressRelease(Settings.FireGolemKeyPressed.Value);
                 }
-                if (Settings.IceGolem.Value && countIceGolem < Settings.IceGolemMax.Value)
+                if (Settings.IceGolem.Value && countIceGolem < Settings.IceGolemMax.Value && ingameUiElements.SkillBar[Settings.IceGolemConnectedSkill.Value - 1].SkillIconPath.Contains("IceElementalSummon"))
                 {
                     keyboard.KeyPressRelease(Settings.IceGolemKeyPressed.Value);
                 }
-                if (Settings.LightningGolem.Value && countLightningGolem < Settings.LightningGolemMax.Value)
+                if (Settings.LightningGolem.Value && countLightningGolem < Settings.LightningGolemMax.Value && ingameUiElements.SkillBar[Settings.LightningGolemConnectedSkill.Value - 1].SkillIconPath.Contains("LightningGolem"))
                 {
                     keyboard.KeyPressRelease(Settings.LightningGolemKeyPressed.Value);
                 }
-                if (Settings.StoneGolem.Value && countStoneGolem < Settings.StoneGolemMax.Value)
+                if (Settings.StoneGolem.Value && countStoneGolem < Settings.StoneGolemMax.Value && ingameUiElements.SkillBar[Settings.StoneGolemConnectedSkill.Value - 1].SkillIconPath.Contains("RockGolemSummon"))
                 {
                     keyboard.KeyPressRelease(Settings.StoneGolemKeyPressed.Value);
                 }
--- src/AutoGolemSettings.cs
+++ src/AutoGolemSettings.cs
@@ -14,59 +14,74 @@ namespace AutoGolem
             //Golems
             ChaosGolem = false;
             ChaosGolemKeyPressed = Keys.T;
+            ChaosGolemConnectedSkill = new RangeNode<int>(1, 1, 8);
             ChaosGolemMax = new RangeNode<int>(1, 1, 10);
 
             FireGolem = false;
             FireGolemKeyPressed = Keys.T;
+            FireGolemConnectedSkill = new RangeNode<int>(1, 1, 8);
             FireGolemMax = new RangeNode<int>(1, 1, 10);
 
             IceGolem = false;
             IceGolemKeyPressed = Keys.T;
+            IceGolemConnectedSkill = new RangeNode<int>(1, 1, 8);
             IceGolemMax = new RangeNode<int>(1, 1, 10);
 
             LightningGolem = false;
             LightningGolemKeyPressed = Keys.T;
+            LightningGolemConnectedSkill = new RangeNode<int>(1, 1, 8);
             LightningGolemMax = new RangeNode<int>(1, 1, 10);
 
             StoneGolem = false;
             StoneGolemKeyPressed = Keys.T;
+            StoneGolemConnectedSkill = new RangeNode<int>(1, 1, 8);
             StoneGolemMax = new RangeNode<int>(1, 1, 10);
         }
 
         //Menu
         [Menu("Chaos Golem", 1)]
         public ToggleNode ChaosGolem { get; set; }
-        [Menu("Skill Hotkey", 10, 1)]
+        [Menu("Skill Hotkey", "Set the hotkey of the Summon Chaos Golem skill", 10, 1)]
         public HotkeyNode ChaosGolemKeyPressed { get; set; }
-        [Menu("Max Golems", 11, 1)]
+        [Menu("Connected Skill", "Set the skill slot (1 = top left, 8 = bottom right)", 11, 1)]
+        public RangeNode<int> ChaosGolemConnectedSkill { get; set; }
+        [Menu("Max Golems", "Maximum number of golems to summon for this type", 12, 1)]
         public RangeNode<int> ChaosGolemMax { get; set; }
 
-        [Menu("Fire Golem", 2)]
+        [Menu("Flame Golem", 2)]
         public ToggleNode FireGolem { get; set; }
-        [Menu("Skill Hotkey", 20, 2)]
+        [Menu("Skill Hotkey", "Set the hotkey of the Summon Flame Golem skill", 20, 2)]
         public HotkeyNode FireGolemKeyPressed { get; set; }
-        [Menu("Max Golems", 21, 2)]
+        [Menu("Connected Skill", "Set the skill slot (1 = top left, 8 = bottom right)", 21, 2)]
+        public RangeNode<int> FireGolemConnectedSkill { get; set; }
+        [Menu("Max Golems", "Maximum number of golems to summon for this type", 22, 2)]
         public RangeNode<int> FireGolemMax { get; set; }
 
         [Menu("Ice Golem", 3)]
         public ToggleNode IceGolem { get; set; }
-        [Menu("Skill Hotkey", 30, 3)]
+        [Menu("Skill Hotkey", "Set the hotkey of the Summon Ice Golem skill", 30, 3)]
         public HotkeyNode IceGolemKeyPressed { get; set; }
-        [Menu("Max Golems", 31, 3)]
+        [Menu("Connected Skill", "Set the skill slot (1 = top left, 8 = bottom right)", 31, 3)]
+        public RangeNode<int> IceGolemConnectedSkill { get; set; }
+        [Menu("Max Golems", "Maximum number of golems to summon for this type", 32, 3)]
         public RangeNode<int> IceGolemMax { get; set; }
 
         [Menu("Lightning Golem", 4)]
         public ToggleNode LightningGolem { get; set; }
-        [Menu("Skill Hotkey", 40, 4)]
+        [Menu("Skill Hotkey", "Set the hotkey of the Summon Lightning Golem skill", 40, 4)]
         public HotkeyNode LightningGolemKeyPressed { get; set; }
-        [Menu("Max Golems", 41, 4)]
+        [Menu("Connected Skill", "Set the skill slot (1 = top left, 8 = bottom right)", 41, 4)]
+        public RangeNode<int> LightningGolemConnectedSkill { get; set; }
+        [Menu("Max Golems", "Maximum number of golems to summon for this type", 42, 4)]
         public RangeNode<int> LightningGolemMax { get; set; }
 
         [Menu("Stone Golem", 5)]
         public ToggleNode StoneGolem { get; set; }
-        [Menu("Skill Hotkey", 50, 5)]
+        [Menu("Skill Hotkey", "Set the hotkey of the Summon Stone Golem skill", 50, 5)]
         public HotkeyNode StoneGolemKeyPressed { get; set; }
-        [Menu("Max Golems", 51, 5)]
+        [Menu("Connected Skill", 51, 5)]
+        public RangeNode<int> StoneGolemConnectedSkill { get; set; }
+        [Menu("Max Golems", "Maximum number of golems to summon for this type", 52, 5)]
         public RangeNode<int> StoneGolemMax { get; set; }
     }
 }
